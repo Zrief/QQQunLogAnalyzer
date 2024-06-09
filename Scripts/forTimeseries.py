e790b7507353fb06f031da87e2f8de4a8cd0cf4f
Zrief <103112786+Zrief@users.noreply.github.com>
2024-06-09 22:17:04 +0800

    绘图美化

--- Scripts/forTimeseries.py
+++ Scripts/forTimeseries.py
@@ -7,7 +7,6 @@ import os
 
 plt.rcParams['font.sans-serif'] = ['Simhei']  #显示中文
 plt.rcParams['axes.unicode_minus'] = False    #显示负号
-
 # 创建OutputTimeseries文件夹，如果不存在
 if not os.path.exists('OutputTimeseries'):
     os.makedirs('OutputTimeseries')
@@ -30,7 +29,7 @@ message_count = df['Sender'].value_counts()
 # 统计每个用户在各个小时发送的消息数量
 message_count_by_hour = df.groupby(['Sender', 'Hour']).size().reset_index(name='Counts')
 
-# 如果sender数量大于20，则只展示前20个
+# 如果sender数量大于30，则只展示前30个
 if len(message_count) > 20:
     top_senders = message_count.index[:20]
     message_count = message_count.loc[top_senders]
@@ -39,23 +38,26 @@ if len(message_count) > 20:
 # 根据时间范围确定图的宽度
 time_range = df['Time'].max() - df['Time'].min()
 width = max(10, time_range.days / 30)  # 每30天增加1个单位的宽度
+barwidth = len(message_count)/2
 
 # 绘制每个用户发送的消息总数的柱状图
-plt.figure(figsize=(width, 5))
-sns.barplot(x=message_count.index, y=message_count.values)
-plt.title('Message Counts by Sender')
-plt.xlabel('Sender')
-plt.ylabel('Count')
-plt.xticks(rotation=90)
+plt.figure(dpi=300,figsize=(barwidth, 10))
+plt.grid()
+sns.barplot(y=message_count.index, x=message_count.values,hue=message_count.index)
+plt.title('水群榜（不准确仅供娱乐）',fontsize=20)
+plt.xticks(rotation=0,fontsize=15)
 plt.tight_layout()
-plt.savefig('OutputTimeseries/MessageCountsBySender.png')
+
+ax = plt.gca()
+ax.spines.right.set_color('none')
+ax.spines['top'].set_visible(False)
+plt.savefig('OutputTimeseries/柱状图.png')
 
 # 绘制每个用户在各个小时发送的消息数量的热力图
-message_count_by_hour_pivot = message_count_by_hour.pivot('Sender', 'Hour', 'Counts').fillna(0)
-plt.figure(figsize=(width, 5))
-sns.heatmap(message_count_by_hour_pivot, cmap='Blues')
-plt.title('Message Counts by Sender and Hour')
+message_count_by_hour_pivot = message_count_by_hour.pivot(index='Sender', columns='Hour', values='Counts').fillna(0)
+plt.figure(dpi=300,figsize=(width, 10))
+sns.heatmap(message_count_by_hour_pivot, cmap='bone_r')
+plt.title('消息数热力图')
 plt.xlabel('Hour')
-plt.ylabel('Sender')
 plt.tight_layout()
-plt.savefig('OutputTimeseries/MessageCountsBySenderAndHour.png')
+plt.savefig('OutputTimeseries/热力图.png')
